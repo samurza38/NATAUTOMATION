15a5bef40bb400ec9a624828491dc8e358d2fb29
samurza38 <smurza9@gmail.com>
2019-10-08 15:01:59 -0700

    6.th commit

--- src/GitTest1.java
+++ src/GitTest1.java
@@ -2,8 +2,6 @@ public class GitTest1 {
     public static void main(String[]args){
         System.out.println("1");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
+
     }
 }
